f1ab5e7c7795f37d761c0cd46c4a864bfceaae68
vildstein <ng75@mail.ru>
2024-08-03 03:24:55 +0500

    Добавил пару строк в меню и само меню. Пока без функционала

--- LittleFibo/littlefibo.cpp
+++ LittleFibo/littlefibo.cpp
@@ -1,7 +1,18 @@
 #include "littlefibo.h"
 #include "mainwindowscene.h"
 
+#include <QApplication>
 #include <QGraphicsView>
+#include <QAction>
+#include <QMenu>
+#include <QMenuBar>
+#include <QCloseEvent>
+
+
+
+//Удалить потом
+#include <QDebug>
+
 
 LittleFibo::LittleFibo(QWidget *parent)
     : QMainWindow(parent)
@@ -9,10 +20,86 @@ LittleFibo::LittleFibo(QWidget *parent)
     mScene = new MainWindowScene(this);
     mView = new QGraphicsView(mScene, this);
 
+    createActions();
+    createMenus();
+
     setCentralWidget(mView);
+    setWindowTitle(tr("LittleFiboHelper"));
 }
 
 LittleFibo::~LittleFibo()
 {
 
+}
+
+void LittleFibo::createFileDialog()
+{
+
+}
+
+void LittleFibo::createDataLoadFromNetworkDialog()
+{
+
+}
+
+void LittleFibo::about()
+{
+
+}
+
+void LittleFibo::closeEvent(QCloseEvent *event)
+{
+    saveFile();
+    event -> accept();
+
+}
+
+void LittleFibo::loadFile()
+{
+
+}
+
+void LittleFibo::saveFile()
+{
+
+}
+//Пока так. Доделать.
+bool LittleFibo::loadDataFromFile()
+{
+    return true;
+
+}
+
+void LittleFibo::createActions()
+{
+    readFromFileAction = new QAction(tr("Загрузить данные из файла"), this);
+
+    connectToNetWorkAction = new QAction(tr("Соединиться с сервером"), this);
+
+    exitAction  = new QAction(("Выход"), this);
+
+    aboutAction = new QAction(("Про LittleFibo"), this);
+    aboutQtAction = new QAction(("About Qt"), this);
+
+    //Коннекты для меню "Файл"
+    connect(readFromFileAction, &QAction::triggered, this, &LittleFibo::createFileDialog);
+    connect(connectToNetWorkAction, &QAction::triggered, this, &LittleFibo::createDataLoadFromNetworkDialog);//
+    connect(exitAction, &QAction::triggered, this, &LittleFibo::close);
+    //Коннекты для меню About
+    connect(aboutAction, &QAction::triggered, this, &LittleFibo::about);
+    connect(aboutQtAction, &QAction::triggered, qApp, &QApplication::aboutQt);
+
+
+}
+
+void LittleFibo::createMenus()
+{
+    fileMenu = menuBar() -> addMenu(tr("Файл"));
+    fileMenu -> addAction(readFromFileAction);
+    fileMenu -> addAction(connectToNetWorkAction);
+    fileMenu -> addAction(exitAction);
+
+    aboutMenu = menuBar() -> addMenu(tr("О программе"));
+    aboutMenu -> addAction(aboutAction);
+    aboutMenu -> addAction(aboutQtAction);
 }
--- LittleFibo/littlefibo.h
+++ LittleFibo/littlefibo.h
@@ -5,6 +5,17 @@
 
 class QGraphicsView;
 class MainWindowScene;
+class QAction;
+class QMenu;
+
+//Сначала прописать класс Bar
+//В Меню сделать 2 варианта: загрузить данные из файла
+// и подключиться к серваку
+//Прочитать торговые данные из файла
+//Сохранить торговые данные в файл
+//Вывести торговые данные на сцену
+//Масштабирование
+//Графические инструменты
 
 class LittleFibo : public QMainWindow
 {
@@ -23,12 +34,35 @@ signals:
 public slots:
 
 private slots:
+    void createFileDialog();
+    void createDataLoadFromNetworkDialog();
+    void about();
 
 protected:
+    virtual void closeEvent(QCloseEvent *event) override;
 
 private:
+    //Функция загрузки данных с которыми работали в последнюю очередь. Вызывается при запуске программы.
+    void loadFile();
+    void saveFile();
+
+    //Функция загрузки данных из файла текстового или файла приложения
+    bool loadDataFromFile();
+
+    void createActions();
+    void createMenus();
+
     QGraphicsView *mView;
     MainWindowScene *mScene;
 
+    QAction *readFromFileAction;
+    QAction *connectToNetWorkAction;
+    QAction *exitAction;
+
+    QAction *aboutAction;
+    QAction *aboutQtAction;
+
+    QMenu *fileMenu;
+    QMenu *aboutMenu;
 };
 #endif // LITTLEFIBO_H
