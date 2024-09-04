61f8c7e09ccb63efd6de904d26fc3d06863aafff
vildstein <ng75@mail.ru>
2024-09-05 03:06:24 +0500

    Написал пару функций. Делаю апгрейд системы. Делаю комит, чтобы апгрейд системы не снёс те пару функций, которые я написал

--- LittleFibo/fibonaccilevels.cpp
+++ LittleFibo/fibonaccilevels.cpp
@@ -2,6 +2,8 @@
 #include "dot.h"
 #include "templates.h"
 
+#include <QApplication>
+
 #include <QPen>
 #include <QDebug>
 #include <QPainter>
@@ -55,17 +57,17 @@ FibonacciLevels::FibonacciLevels(Dot *startPos, Dot *endPos, QGraphicsItem *pare
 
     mLevel4263Line = new QLineF;
 
-    mLinePointersMap.insert(1.0, mLevelOneLine);
-    mLinePointersMap.insert(0.0, mLevelZeroLine);
-    mLinePointersMap.insert(0.236, mLevel0236Line);
-    mLinePointersMap.insert(0.382, mLevel0382Line);
-    mLinePointersMap.insert(0.5, mLevel05Line);
-    mLinePointersMap.insert(0.618, mLevel0618Line);
-    mLinePointersMap.insert(0.786, mLevel0786Line);
-    mLinePointersMap.insert(1.272, mLevel1272Line);
-    mLinePointersMap.insert(1.618, mLevel1618Line);
-    mLinePointersMap.insert(2.618, mLevel2618Line);
-    mLinePointersMap.insert(4.618, mLevel4263Line);
+    // mLinePointersMap.insert(1.0, mLevelOneLine);
+    // mLinePointersMap.insert(0.0, mLevelZeroLine);
+    // mLinePointersMap.insert(0.236, mLevel0236Line);
+    // mLinePointersMap.insert(0.382, mLevel0382Line);
+    // mLinePointersMap.insert(0.5, mLevel05Line);
+    // mLinePointersMap.insert(0.618, mLevel0618Line);
+    // mLinePointersMap.insert(0.786, mLevel0786Line);
+    // mLinePointersMap.insert(1.272, mLevel1272Line);
+    // mLinePointersMap.insert(1.618, mLevel1618Line);
+    // mLinePointersMap.insert(2.618, mLevel2618Line);
+    // mLinePointersMap.insert(4.618, mLevel4263Line);
 
     createActions();
     createMenus();
@@ -76,27 +78,39 @@ FibonacciLevels::FibonacciLevels(Dot *startPos, Dot *endPos, QGraphicsItem *pare
 FibonacciLevels::~FibonacciLevels()
 {
     delete mMainLine;
-    //Шаблонная функция. См. файл templates.h
-    TemplatesFunctions::clearDynamicMap(mLinePointersMap);
-    //TemplatesFunctions::clearDynamicVector(mQActionVector);
+    delete mLevelOneLine;
+    delete mLevelZeroLine;
+    delete mLevel0236Line;
+    delete mLevel0382Line;
+    delete mLevel05Line;
+    delete mLevel0618Line;
+    delete mLevel0786Line;
+    delete mLevel1272Line;
+    delete mLevel1618Line;
+    delete mLevel2618Line;
+    delete mLevel4263Line;
 
-    if(!mQActionVector.isEmpty())
-    {
-        QVector<QAction*>::iterator i;
-        for(i = mQActionVector.begin(); i!=mQActionVector.end(); ++i)
-        {
-            delete *i;
-            *i=nullptr;
-        }
-        mQActionVector.clear();
 
-    }
-    qInfo() << "Destructor Of FiboLevels worked";
+    delete setupAction;
+
+
+    delete showZeroLevelAction;
+    delete show0236LevelAction;
+    delete show0382LevelAction;
+    delete show05LevelAction;
+    delete show0618LevelAction;
+    delete show786LevelAction;
+    delete showOneLevelAction;
+
+    delete deleteFiboLevelsAction;
+    delete fiboLevelsContextMenu;
+
 }
 
 void FibonacciLevels::updatePosition()
 {
     mMainLine -> setPoints(mapFromItem(mStartPosDot, 0, 0), mapFromItem(mEndPosDot, 0, 0));
+    //mMainLine -> setPoints(mStartPosDot -> getCenter(), mEndPosDot -> getCenter());
     mLevelOneLine -> setPoints(mMainLine -> p1(), QPointF(mMainLine -> p2().x(), mMainLine -> p1().y()));
     mLevelZeroLine -> setPoints(mMainLine -> p2(), QPointF(mMainLine -> p1().x(), mMainLine -> p2().y()));
     mLevel0236Line -> setPoints(QPointF(mMainLine -> p1().x(), mMainLine -> pointAt(0.236).y()), QPointF(mMainLine -> p2().x(), mMainLine -> pointAt(0.236).y()));
@@ -107,6 +121,16 @@ void FibonacciLevels::updatePosition()
 
     setLine(*mMainLine);
 }
+
+QPainterPath FibonacciLevels::shape() const
+{
+    QPainterPath path;
+    path.addRect(QRectF(mMainLine -> p1(), mMainLine -> p2()).adjusted(-1, -1, +1, +1));
+
+    return path;
+}
+
+
 
 // QVector<QLineF> &FibonacciLevels::getLineVector()
 // {
@@ -188,37 +212,38 @@ void FibonacciLevels::paint(QPainter *painter, const QStyleOptionGraphicsItem *o
 
 }
 
-void FibonacciLevels::mousePressEvent(QGraphicsSceneMouseEvent *event)
-{
-    this -> setSelected(true);
-    mStartPosDot -> setSelected(true);
-    mEndPosDot -> setSelected(true);
-    QGraphicsLineItem::mousePressEvent(event);
-}
 void FibonacciLevels::createActions()
 {
+    QFont font("Times", 12, QFont::Bold);
     setupAction = new QAction(QObject::tr("Настроить уровни Фибоначчи"), this);
 
     showZeroLevelAction = new QAction(QObject::tr("Уровень 0.0"), this);
+    showZeroLevelAction -> setFont(font);
     show0236LevelAction = new QAction(QObject::tr("Уровень 0.236"), this);
     show0382LevelAction = new QAction(QObject::tr("Уровень 0.382"), this);
     show05LevelAction = new QAction(QObject::tr("Уровень 0.5"), this);
     show0618LevelAction = new QAction(QObject::tr("Уровень 0.618"), this);
     show786LevelAction = new QAction(QObject::tr("Уровень 0.786"), this);
     showOneLevelAction = new QAction(QObject::tr("Уровень 1.0"), this);
+    showOneLevelAction -> setFont(font);
 
     deleteFiboLevelsAction = new QAction(QObject::tr("Удалить эти уровни Фибоначчи"), this);
 
-    mQActionVector.append(std::move(setupAction));
-    mQActionVector.append(std::move(showZeroLevelAction));
-    mQActionVector.append(std::move(show0236LevelAction));
-    mQActionVector.append(std::move(show0382LevelAction));
-    mQActionVector.append(std::move(show05LevelAction));
-    mQActionVector.append(std::move(show0618LevelAction));
-    mQActionVector.append(std::move(show786LevelAction));
-    mQActionVector.append(std::move(showOneLevelAction));
 
-    mQActionVector.append(std::move(deleteFiboLevelsAction));
+    //Показываем или скрываем уровень 0.0
+    connect(showZeroLevelAction, &QAction::triggered, this, [this]() mutable { showOrHideFiboLevels(Functions::setInvertedBool, isLevelZeroLine, showZeroLevelAction) ;});
+    //Показываем или скрываем уровень 0.236
+    connect(show0236LevelAction, &QAction::triggered, this, [this]() mutable { showOrHideFiboLevels(Functions::setInvertedBool, isLevel0236Line, show0236LevelAction) ;});
+    //Показываем или скрываем уровень 0.382
+    connect(show0382LevelAction, &QAction::triggered, this, [this]() mutable { showOrHideFiboLevels(Functions::setInvertedBool, isLevel0382Line, show0382LevelAction) ;});
+    //Показываем или скрываем уровень 0.5
+    connect(show05LevelAction, &QAction::triggered, this, [this]() mutable { showOrHideFiboLevels(Functions::setInvertedBool, isLevel05Line, show05LevelAction) ;});
+    //Показываем или скрываем уровень 0.618
+    connect(show0618LevelAction, &QAction::triggered, this, [this]() mutable { showOrHideFiboLevels(Functions::setInvertedBool, isLevel0618Line, show0618LevelAction) ;});
+    //Показываем или скрываем уровень 0.786
+    connect(show786LevelAction, &QAction::triggered, this, [this]() mutable { showOrHideFiboLevels(Functions::setInvertedBool, isLevel0786Line, show786LevelAction) ;});
+    //Показываем или скрываем уровень 0.786
+    connect(showOneLevelAction, &QAction::triggered, this, [this]() mutable { showOrHideFiboLevels(Functions::setInvertedBool, isLevelOneLine, showOneLevelAction) ;});
 }
 
 void FibonacciLevels::createMenus()
@@ -237,19 +262,111 @@ void FibonacciLevels::createMenus()
 
     fiboLevelsContextMenu -> addSeparator();
     fiboLevelsContextMenu -> addAction(deleteFiboLevelsAction);
-
+    //connect(fiboLevelsContextMenu, &QMenu::aboutToHide, this, [this](){ scene() -> clearSelection();});
+    //connect(fiboLevelsContextMenu, &QMenu::aboutToHide, this, [this](){ selectAllItems();});
+    connect(fiboLevelsContextMenu, &QMenu::aboutToHide, this, [this](){ unSelectAllItems();});
 }
-void FibonacciLevels::contextMenuEvent(QGraphicsSceneContextMenuEvent *event)
+//Если флаг на видимость линии стоит в false
+//инвентируем на true
+//Выделяем показанную линию утолщённым шрифтом
+//Потом нужно будет доделать иконки как checked и unchecked
+
+//Первый аргумент - это указатель на функцию
+//не знаю эффективно или нет, решил написать, чтобы вспомнить
+void FibonacciLevels::showOrHideFiboLevels(void (*iverterFunction)(bool&), bool &levelFlag, QAction *action)
 {
-    scene() -> clearSelection();
-    setSelected(true);
-    fiboLevelsContextMenu -> popup(event -> screenPos());
-
+    QFont font("Times", 12, QFont::Bold);
+    if(!levelFlag)
+    {
+        iverterFunction(levelFlag);
+        action -> setFont(font);
+    }
+    else
+    {
+        iverterFunction(levelFlag);
+        font = qApp -> font();
+        action -> setFont(font);
+    }
 }
-void FibonacciLevels::mouseReleaseEvent(QGraphicsSceneMouseEvent *event)
+
+void FibonacciLevels::selectAllItems()
+{
+    this -> setSelected(true);
+    mStartPosDot -> setSelected(true);
+    mEndPosDot -> setSelected(true);
+    //updatePosition();
+}
+
+void FibonacciLevels::unSelectAllItems()
 {
     this -> setSelected(false);
     mStartPosDot -> setSelected(false);
     mEndPosDot -> setSelected(false);
+}
+void FibonacciLevels::contextMenuEvent(QGraphicsSceneContextMenuEvent *event)
+{
+    scene() -> clearSelection();
+    //setSelected(true);
+    selectAllItems();
+    fiboLevelsContextMenu -> popup(event -> screenPos());
+    //QGraphicsLineItem::contextMenuEvent(event);
+}
+
+void FibonacciLevels::mousePressEvent(QGraphicsSceneMouseEvent *event)
+{
+    if(event -> button() == Qt::LeftButton)
+    {
+        // this -> setSelected(true);
+        // mStartPosDot -> setSelected(true);
+        // mEndPosDot -> setSelected(true);
+        //mStartPosDot -> setPos(mMainLine -> p1());
+        //mEndPosDot -> setPos(mMainLine -> p2());
+        //this -> updatePosition();
+        mStartPosDot -> setPos(mStartPosDot -> pos());
+        mEndPosDot -> setPos(mEndPosDot -> pos());
+        updatePosition();
+        selectAllItems();
+        //updatePosition();
+        QGraphicsLineItem::mousePressEvent(event);
+    }
+}
+
+QVariant FibonacciLevels::itemChange(GraphicsItemChange change, const QVariant &value)
+{
+    if (change == ItemPositionChange && scene())
+    {
+        selectAllItems();
+        updatePosition();
+    }
+    return QGraphicsItem::itemChange(change, value);
+}
+
+void FibonacciLevels::mouseReleaseEvent(QGraphicsSceneMouseEvent *event)
+{
+    if(event -> button() == Qt::LeftButton)
+    {
+        // this -> setSelected(false);
+        // mStartPosDot -> setSelected(false);
+        // mEndPosDot -> setSelected(false);
+        //
+        //scene() -> clearSelection();
+        //QGraphicsLineItem::mouseReleaseEvent(event);
+        unSelectAllItems();
+        updatePosition();
+    }
+    //scene() -> clearSelection();
+
     QGraphicsLineItem::mouseReleaseEvent(event);
+}
+
+void FibonacciLevels::mouseMoveEvent(QGraphicsSceneMouseEvent *event)
+{
+    //mStartPosDot -> setPos(mMainLine -> p1());
+    //mEndPosDot -> setPos(mMainLine -> p2());
+    //this -> updatePosition();
+    // mStartPosDot -> setPos(mStartPosDot -> pos());
+    // mEndPosDot -> setPos(mEndPosDot -> pos());
+    selectAllItems();
+    updatePosition();
+    QGraphicsLineItem::mouseMoveEvent(event);
 }
--- LittleFibo/fibonaccilevels.h
+++ LittleFibo/fibonaccilevels.h
@@ -19,12 +19,21 @@ public:
 
     //Сеттеры
 
+signals:
+
+public slots:
+
+private slots:
+
+
+
 protected:
     //virtual QRectF boundingRect() const override;
-    //virtual QPainterPath shape() const override;
+    virtual QPainterPath shape() const override;
     virtual void paint(QPainter *painter, const QStyleOptionGraphicsItem *option, QWidget *widget = nullptr) override;
+    virtual void mouseMoveEvent(QGraphicsSceneMouseEvent *event) override;
     virtual void mousePressEvent(QGraphicsSceneMouseEvent *event) override;
-    //virtual QVariant itemChange(QGraphicsItem::GraphicsItemChange change, const QVariant &value) override;
+    virtual QVariant itemChange(QGraphicsItem::GraphicsItemChange change, const QVariant &value) override;
     virtual void mouseReleaseEvent(QGraphicsSceneMouseEvent *event) override;
     virtual void contextMenuEvent(QGraphicsSceneContextMenuEvent *event) override;
 
@@ -33,6 +42,13 @@ private:
     void createActions();
     //Создать само Меню
     void createMenus();
+    //Показать или скрыть уровни Фибоначчи
+    void showOrHideFiboLevels(void (*iverterFunction)(bool&), bool &levelFlag, QAction *action);
+
+    void selectAllItems();
+    void unSelectAllItems();
+
+
     //Стартовые точки
     Dot *mStartPosDot;
     Dot *mEndPosDot;
